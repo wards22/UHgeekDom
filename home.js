8235d1da765dc96d10f8d4c7cdcd3b0932996953
wards22 <scott.harrison.ward@gmail.com>
2024-11-19 02:43:19 -0600

    Fixed responsive issue for mobile devices

--- home.js
+++ home.js
@@ -2,6 +2,22 @@ function themeController() {
 
 }
 
+const menuBtn = document.querySelector('.right-bar-menu')
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+const list = document.querySelector('.right-bar-items');
+menuBtn.addEventListener('click', () => {
+    list.classList.toggle('show');
+});
+
 const qq = document.querySelector(".dropdown-content");
 function s() {
     qq.style.display = "flex";
